29ba41e52d8c1aa6921c32a8a7f248f6453c601f
Aledend <theodor.brandt@hotmail.com>
2020-12-06 22:41:47 +0100

    Initial commit

---
+++ Source/NetworkingCourse/NetworkingCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NetworkingCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NetworkingCourse, "NetworkingCourse" );
---
+++ Source/NetworkingCourse/NetworkingCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkingCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKINGCOURSE_API ANetworkingCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
